7de8db1214178f07cee8d94515cb9179826b858c
zhengmao <zhengmao0613@163.com>
2022-08-04 13:40:29 +0800

    store theme to localstorage

--- themes/marin/static/js/index.js
+++ themes/marin/static/js/index.js
@@ -1,7 +1,29 @@
-function toggleTheme () {
-  if (document.body.classList.contains('theme-light')) {
-    document.body.classList.replace('theme-light', 'theme-dark')
+const THEME_STORAGE_KEY = "theme"
+
+function toggleCSS(el, firstCSS, secondCSS = "") {
+  if (el.classList.contains(firstCSS)) {
+    el.classList.replace(firstCSS, secondCSS)
   } else {
-    document.body.classList.replace('theme-dark', 'theme-light')
+    el.classList.remove(secondCSS)
+    el.classList.add(firstCSS)
   }
 }
+
+let theme = "light"
+function toggleTheme () {
+  theme = theme == "light" ? "dark" : "light"
+
+  const icon = document.querySelector('#theme-toggle i')
+
+  toggleCSS(document.body, "theme-light", "theme-dark")
+  toggleCSS(icon, "fa-moon-o", "fa-sun-o")
+
+  localStorage.setItem(THEME_STORAGE_KEY, theme)
+}
+
+function initTheme() {
+  if (localStorage.getItem(THEME_STORAGE_KEY) == "dark") {
+    toggleTheme()
+  }
+}
+initTheme()
